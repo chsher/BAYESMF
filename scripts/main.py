dc460a2c8759784b454626202dbba6bc5eccd390
chsher <schaoharvard@gmail.com>
2020-05-07 17:13:39 +0000

    working on cmf with svi

--- scripts/main.py
+++ scripts/main.py
@@ -20,16 +20,17 @@ if __name__ == "__main__":
     #X, _ = make_insilico_dataset()
     X, _ = make_downsampled_dataset()
     
-    errs, durs = run_kfold_xval(X, kfold=10, random_state=22690, init=None, components = [10, 15, 20, 25, 30], 
+    errs, durs = run_kfold_xval(X, kfold=10, random_state=22690, init=None, components = [10, 15, 20], 
                                 methods = ['nmf-vanilla', 'nmf-consensus', 'lda-batch', 'lda-stochastic',
                                            'bmf-batch', 'bmf-stochastic', 'cmf-batch'])
     
     pickle.dump(errs, open(os.path.join(DIR_PATH, 'd_errs_' + handle + '.pkl'), 'wb'))
     pickle.dump(durs, open(os.path.join(DIR_PATH, 'd_durs_' + handle + '.pkl'), 'wb'))
     
-    errs, durs = run_kfold_xval(X, kfold=10, random_state=22690, init='nmf', components = [10, 15, 20, 25, 30], 
+    errs, durs = run_kfold_xval(X, kfold=10, random_state=22690, init='nmf', components = [10, 15, 20], 
                                 methods = ['nmf-vanilla', 'nmf-consensus', 'lda-batch', 'lda-stochastic',
                                            'bmf-batch', 'bmf-stochastic', 'cmf-batch'])
     
     pickle.dump(errs, open(os.path.join(DIR_PATH, 'd_errs_nmf_' + handle + '.pkl'), 'wb'))
     pickle.dump(durs, open(os.path.join(DIR_PATH, 'd_durs_nmf_' + handle + '.pkl'), 'wb'))
+    
